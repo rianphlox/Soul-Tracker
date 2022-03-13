0455bfa371d06c97e5f2982de632b8fbaf3a9a37
rianphlox <skellybrownston@gmail.com>
2022-03-13 03:28:14 +0100

    Latest Commit

--- track.py
+++ track.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import *
 from random import choice, sample
 from config import *
-
+from selenium.webdriver.support.ui import WebDriverWait
 
 def handleReload(): 
     pg.click(753, 600)
@@ -36,6 +36,7 @@ def track():
 options = Options()
 options.add_argument('--start-maximized')
 driver = webdriver.Chrome(options=options)
+
 driver.get(url)
 
 driver.find_element(By.XPATH, '//*[@id="loginUsername"]').clear()
@@ -52,27 +53,42 @@ sleep(2)
 
 handleReload()
 
-trackBtns = driver.find_elements(By.LINK_TEXT, 'Track')
-for btn in trackBtns:
-    btn.click()
-    driver.implicitly_wait(3)
-    
-    
-    
-
-# count = 0
-# while count < len(trackBtns):
-#     trackBtns[count].click()
-#     driver.find_element(By.NAME, "email").clear()
-#     driver.find_element(By.NAME, "email").send_keys("adewole@gmail.com")
-
-#     driver.find_element(By.NAME, 'cell_name').clear()
-#     driver.find_element(By.NAME, 'cell_name').send_keys(cell_name)
-
-#     Select(driver.find_element(By.NAME, 'church_message')).select_by_value('4')
-
-#     Select(driver.find_element(By.NAME, 'foundation_message')).select_by_value('7')
-
-#     driver.find_element_by_xpath("//button[@type='submit']").click()
-
-#     count += 1
+# len_of_list = len(driver.find_elements(By.LINK_TEXT, 'Track'))
+# xpaths = "xpath=//table[@id='mytable']/tbody/tr[{count}]/td[7]/a"
+# for count, btn in enumerate(trackBtns, start=1):
+#     btn.click()
+#     driver.implicitly_wait(3)
+#     track()
+tbody = driver.execute_script("return document.querySelector('tbody')")
+# print(f"{tbody} was returned")
+trs = tbody.find_elements(By.TAG_NAME, 'tr')
+# trs[0].find
+# print(len(trs), 'was found')
+tds = trs[0].find_elements(By.TAG_NAME, 'td')
+# print(len(tds), 'were found')
+a = tds[len(tds) - 1].find_element(By.TAG_NAME, 'a')
+a.click()
+driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.ESCAPE)
+try:
+    modal = driver.find_element(By.CLASS_NAME, 'modal')
+    try:
+        modal_dialog = modal.find_element(By.CLASS_NAME, 'modal-dialog')
+        try:
+            modal_content = modal_dialog.find_element(By.CLASS_NAME ,'modal-content')
+            print('modal-content was found')
+            try:
+                modal_body = modal_content.find_element(By.CLASS_NAME, 'modal-body')
+                try:
+                    rows = modal_body.find_elements(By.XPATH, )
+                    print(len(rows), 'rows were found')
+                    # print(rows[0].text)
+                except:
+                    print('No such element as row')
+            except:
+                print('Modal Body not found')
+        except:
+            print("Modal Content not found")
+    except NoSuchElementException:
+        print("Modal Dialog not found")
+except NoSuchElementException:
+    print('No such element found')
